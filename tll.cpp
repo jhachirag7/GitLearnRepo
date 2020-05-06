9a9d03259e0a597fd71e1252223067cccb9f6982
LoukikNaik <loukiknaik@gmail.com>
2020-05-07 03:25:30 +0530

    tll added

---
+++ tll.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include <string>
+#include <iostream>
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("output.txt", "w", stdout);
+	freopen("input.txt", "r", stdin);
+#endif
+	fastio
+	
+	return 0;
+}
+	
